1960303dc40186a3e439e0713757dd52c6904e07
= <=>
2022-11-07 10:39:02 +0100

    svelte project setup

---
+++ svelte/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Counter from './lib/Counter.svelte';
+</script>
+
+<main class="text-center">
+	<h1 class="p-3 text-lg font-bold">Hello Typescript!</h1>
+
+	<Counter />
+
+	<Counter />
+
+	<p class="p-2 mt-10 text-sm">
+		Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
+	</p>
+
+	<p class="p-2 text-sm text-gray-500">
+		Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported
+		framework, also powered by Vite!
+	</p>
+</main>
+
+<style>
+</style>
---
+++ svelte/tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	darkmode: true,
+	content: ["./out/index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
